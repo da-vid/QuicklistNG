5d2dbf49d832cf60f62ff99963c022e5a9791aef
da-vid <dave.wong@gmail.com>
2014-03-23 22:39:50 -0700

    Dev folder created

---
+++ Dev/js/idHelper.js
@@ -0,0 +1,32 @@
+
+function makeID()
+{   //http://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
+    var text = "";
+    var possible = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789";
+
+    for( var i=0; i < 6; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function getID()
+{   
+    //strip the leading slash first
+    var id = window.location.pathname.substr(1);
+    //strip a trailing slash, if it exists
+    if (id.charAt(id.length - 1) === "/") {
+        window.location.href = "../" + id.substr(0, id.length - 1);
+    }
+    return id;
+}
+
+function getListID() {    
+    var id = getID();
+    if(id) {
+        return id;
+    }
+    else {
+        window.location.href = makeID();
+    }
+}
---
+++ Dev/js/idHelperLocal.js
@@ -0,0 +1,37 @@
+
+function makeID()
+{   //http://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 6; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function getQS()
+{   //http://stackoverflow.com/a/3855394
+    var qs = (function(a) {
+        if (a === "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=');
+            if (p.length != 2) continue;
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'));
+    return qs;
+}
+
+function getListID() {    
+    var qs = getQS();
+    if(qs.i) {
+        return qs.i;
+    }
+    else {
+        window.location.href = "index.html?i="+makeID();
+    }
+}
